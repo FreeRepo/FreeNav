fe1cdf5bf4bb3bf51d39393120843a644a36b076
yeke <yeke@freexue.com>
2012-07-21 17:23:04 +0800

    Try

---
+++ TestPHP/screenShot.php
@@ -0,0 +1,11 @@
+<?php
+$browser = new COM("InternetExplorer.Application");
+$handle = $browser->HWND;
+$browser->Visible = true;
+$im = imagegrabwindow($handle);
+$browser->Quit();
+imagepng($im, "iesnap.png");
+$im = imagegrabscreen();
+?>
+
+
